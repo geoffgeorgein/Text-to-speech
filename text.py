ccd484b3a0541e00ee75c77cc579400f9749e9a4
Geoff George <52931060+geoffgeorgein@users.noreply.github.com>
2020-09-18 15:42:03 +0530

    Update text.py

--- text.py
+++ text.py
@@ -14,3 +14,13 @@ text = pytesseract.image_to_string(image)
 
 # Printing the text
 print(text)
+
+voice_text = ""
+
+# Converts the text into a single line of text
+
+for i in text.split():
+    voice_text += i + ' '
+
+voice_text = voice_text[:-1]
+print(voice_text)
