d0368d396e2c67db80a05c9a7b6f35210d12a63c
Geoff George <52931060+geoffgeorgein@users.noreply.github.com>
2020-09-18 16:02:03 +0530

    added gtts to convert text to speech

--- text.py
+++ text.py
@@ -2,6 +2,8 @@
 import cv2
 import pytesseract
 from PIL import Image
+from gtts import gTTS
+import os
 
 # Specifying the path
 pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe'
@@ -24,3 +26,7 @@ for i in text.split():
 
 voice_text = voice_text[:-1]
 print(voice_text)
+
+tts = gTTS(voice_text)
+tts.save("test.mp3")
+os.system('test.mp3')
